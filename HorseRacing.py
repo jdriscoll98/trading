cec774063847fe865aaa1516e3dc3fc916a30ba5
Jack Driscoll <jdriscoll98@ufl.edu>
2022-03-26 13:28:37 -0400

    requirements

--- HorseRacing.py
+++ HorseRacing.py
@@ -1,140 +1,270 @@
-from pprint import pprint
+import os
 import time
 import requests
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-# read constituents.csv and return a list of tuples
+DAY_1 = 86400
+VERBOSE = False
+INTERACTIVE = False
+DATA_DIR = "data"
 
 
 def read_constituents():
-    with open('constituents.csv', 'r') as f:
+    with open("constituents.csv", "r") as f:
         lines = f.readlines()
-    lines = [line.strip().split(',') for line in lines]
+    lines = [line.strip().split(",") for line in lines]
     return lines
 
 
-def fetch_data():
+def fetch_data(start, end):
+    print("Reading list of symbols...")
     stocks = read_constituents()
 
-    URL = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1=1490486400&period2=1648252800&interval=1d&events=history&includeAdjustedClose=true"
+    URL = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={start}&period2={end}&interval=1d&events=history&includeAdjustedClose=true"
 
-    for stock in stocks[1:]:
+    print("Fetching data for {} stocks...".format(len(stocks)))
+    for stock in tqdm(stocks[1:]):
         symbol = stock[0]
-        formattedURL = URL.format(symbol=symbol)
+        formattedURL = URL.format(symbol=symbol, start=start, end=end)
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        }
         r = requests.get(formattedURL, headers=headers)
-        with open("data/" + symbol + '.csv', 'w') as f:
+        with open("data/" + symbol + ".csv", "w") as f:
             f.write(r.text)
 
+        # Rate limiting
         time.sleep(2)
 
 
-def backtest():
+def backtest(win_percent, loss_percent):
     # starting from the second day
     # list all of the stocks that opened 1% or more above the previous close
     # for each stock, calculate the daily return
 
-    # read data
+    print("Getting list of symbols...")
     stocks = read_constituents()
     # read data from stocks
     data = {}
-    for stock in stocks[1:]:
+    for stock in tqdm(stocks[1:]):
         symbol = stock[0]
         data[symbol] = {}
-        with open("data/" + symbol + '.csv', 'r') as f:
+        with open("data/" + symbol + ".csv", "r") as f:
             lines = f.readlines()
-        stock_data = [line.strip().split(',') for line in lines]
+        stock_data = [line.strip().split(",") for line in lines]
         for line in stock_data[1:]:
-            if ('null' in line):
+            if "null" in line:
                 continue
             data[symbol][line[0]] = {
-                'open': float(line[1]),
-                'high': float(line[2]),
-                'low': float(line[3]),
-                'close': float(line[4]),
-                'adj_close': float(line[5]),
-                'volume': float(line[6]),
+                "open": float(line[1]),
+                "high": float(line[2]),
+                "low": float(line[3]),
+                "close": float(line[4]),
+                "adj_close": float(line[5]),
+                "volume": float(line[6]),
             }
 
-    start_timestamp = 1490486400
-    end_timestamp = 1648252800
+    print("Done.")
 
-    # comvert to datetime
-    start_date = time.strftime('%Y-%m-%d', time.localtime(start_timestamp))
-    end_date = time.strftime('%Y-%m-%d', time.localtime(end_timestamp))
+    # get first start date available
+    start_date = min(data[symbol].keys())
+    end_date = max(data[symbol].keys())
 
-    print("start date: " + start_date)
-    print("end date: " + end_date)
+    start = convert_to_timestamp(start_date)
+    end = convert_to_timestamp(end_date)
 
-    # for each day
-    losing_days, winning_days = 0, 0
-    while (start_timestamp < end_timestamp - 86400):
-        horses = []
+    results = []
+
+    print("Running backtest...")
+    pbar = tqdm(total=int((start - end) / DAY_1))
+    while start < end:
         #  add a day
-        start_timestamp += 86400
-        start_date = time.strftime('%Y-%m-%d', time.localtime(start_timestamp))
+        start += DAY_1
+        start_date = convert_to_date(start)
         # list each stock that opened 1% or more above the previous close
-        for stock in stocks[1:]:
-            symbol = stock[0]
-            if (start_date not in data[symbol]):
-                continue
-            # calculate the daily return
-            yesterday = start_timestamp - 86400
-            yesterday_date = time.strftime(
-                '%Y-%m-%d', time.localtime(yesterday))
-            if (yesterday_date in data[symbol]):
-                previous_close = data[symbol][yesterday_date]['close']
-                current_open = data[symbol][start_date]['open']
-                if (current_open > previous_close * 1.01):
-                    horses.append(symbol)
+        horses = get_horses(stocks, data, start_date)
         # for each horse, calclate current current days return
-        for horse in horses:
-            if (start_date not in data[horse]):
-                continue
-            current_open = data[horse][start_date]['open']
-            current_close = data[horse][start_date]['close']
-            current_return = (
-                current_close - current_open) / current_close
-            data[horse][start_date]['return'] = current_return
-
+        calculate_return(data, start_date, horses)
         # for each horse, calculate the total drawdown
-        for horse in horses:
-            if (start_date not in data[horse]):
-                continue
-            current_open = data[horse][start_date]['open']
-            current_low = data[horse][start_date]['low']
-            current_drawdown = (current_open - current_low) / current_open
-            data[horse][start_date]['drawdown'] = current_drawdown
-
-        winning_horses = []
+        calculate_drawdown(data, start_date, horses)
 
         # if the horse has a return greater than 5%, and drawdown is less than 3%, add to winning horses
-        for horse in horses:
-            if (start_date not in data[horse]):
-                continue
-            if (data[horse][start_date]['return'] > 0.05 and data[horse][start_date]['drawdown'] < 0.02):
-                winning_horses.append(horse)
+        winning_horses = get_winning_horses(
+            win_percent, loss_percent, data, start_date, horses
+        )
 
         # print the winning horses
-        print("winning horses on " + start_date + ": " + str(winning_horses))
+        if VERBOSE:
+            print_details(win_percent, data, start_date, horses, winning_horses)
+            print("-" * 20)
 
-        # print daily winning percentage
-        if (len(horses) > 0):
-            winning_percentage = len(winning_horses) / len(horses)
-            print(start_date, ": winning percentage: " + str(winning_percentage))
+        # if there were horses and some won, add to results
+        if horses and winning_horses:
+            results.append(1)
+        elif horses and not winning_horses:
+            results.append(0)
+
+        if INTERACTIVE:
+            # wait for user to continue
+            input("Press Enter to continue...")
         else:
-            print(start_date, ": no horses")
-        if (len(winning_horses) == 0 and len(horses) > 0):
-            losing_days += 1
-            print("no winning horses on " + start_date)
-        else:
-            winning_days += 1
+            pbar.update(1)
+    pbar.close()
 
-    print("winning days: " + str(winning_days))
-    print("losing days: " + str(losing_days))
-    print("winning percent: " + str(winning_days / (winning_days + losing_days)))
+    # for each day that there were horses, how many days had winning horses?
+    print("-" * 20)
+    print("Results:")
+    percent_of_days_with_winning_horses = round((sum(results) / len(results)) * 100, 2)
+    print(f"{percent_of_days_with_winning_horses}% of days had winning horses")
+    print("-" * 20)
 
 
-backtest()
-# fetch_data()
+def print_details(win_percent, data, start_date, horses, winning_horses):
+    print(start_date)
+    print("-" * 20)
+    if horses:
+        print("Stocks up by 1% in premarket:")
+        for horse in horses:
+            if start_date not in data[horse]:
+                continue
+            print(horse + ": " + str(data[horse][start_date]["preMarket"]) + " %")
+    else:
+        print("No stocks up by 1% in premarket")
+
+    if winning_horses:
+        print(f"Stocks went up {win_percent}% after opening:")
+        for horse in winning_horses:
+            if start_date not in data[horse]:
+                continue
+            print(horse + ": " + str(data[horse][start_date]["return"]) + " %")
+    else:
+        print(f"No stocks went up {win_percent}% after opening")
+        print("Return of all stocks:")
+        for horse in horses:
+            if start_date not in data[horse]:
+                continue
+            print(horse + ": " + str(data[horse][start_date]["return"]) + " %")
+
+
+def get_winning_horses(win_percent, loss_percent, data, start_date, horses):
+    winning_horses = []
+    for horse in horses:
+        if start_date not in data[horse]:
+            continue
+        if (
+            data[horse][start_date]["return"] > win_percent
+            and data[horse][start_date]["drawdown"] < loss_percent
+        ):
+            winning_horses.append(horse)
+    return winning_horses
+
+
+def calculate_drawdown(data, start_date, horses):
+    for horse in horses:
+        if start_date not in data[horse]:
+            continue
+        current_open = data[horse][start_date]["open"]
+        current_low = data[horse][start_date]["low"]
+        current_drawdown = ((current_open - current_low) / current_open) * 100
+        data[horse][start_date]["drawdown"] = round(current_drawdown, 2)
+
+
+def calculate_return(data, start_date, horses):
+    for horse in horses:
+        if start_date not in data[horse]:
+            continue
+        current_open = data[horse][start_date]["open"]
+        current_close = data[horse][start_date]["close"]
+        current_return = ((current_close - current_open) / current_open) * 100
+        data[horse][start_date]["return"] = round(current_return, 2)
+
+
+def get_horses(stocks, data, start_date):
+    horses = []
+    for stock in stocks[1:]:
+        symbol = stock[0]
+        if start_date not in data[symbol]:
+            continue
+            # calculate the daily return
+        start_timestamp = convert_to_timestamp(start_date)
+        yesterday_timestamp = start_timestamp - DAY_1
+        yesterday_date = convert_to_date(yesterday_timestamp)
+        if yesterday_date in data[symbol]:
+            previous_close = data[symbol][yesterday_date]["close"]
+            current_open = data[symbol][start_date]["open"]
+            if current_open > previous_close * 1.01:
+                preMarket = ((current_open - previous_close) / previous_close) * 100
+                data[symbol][start_date]["preMarket"] = round(preMarket, 2)
+                horses.append(symbol)
+
+    return horses
+
+
+def get_dates():
+    start_date = input(f"Please enter the start date (YYYY-MM-DD): ({default_start})")
+    if not start_date:
+        start_date = default_start
+    # Ask for end date
+    end_date = input(f"Please enter the end date (YYYY-MM-DD): ({default_end})")
+    if not end_date:
+        end_date = default_end
+    return start_date, end_date
+
+
+def convert_to_timestamp(date):
+    return int(time.mktime(time.strptime(date, "%Y-%m-%d")))
+
+
+def convert_to_date(timestamp):
+    return time.strftime("%Y-%m-%d", time.localtime(timestamp))
+
+
+while True:
+    default_start = "2017-03-24"
+    default_end = "2022-03-25"
+    # Ask user if they want to backtest or get data
+    print("What would you like to do?")
+    print("1. Get data")
+    print("2. Backtest")
+    print("3. Exit")
+
+    # get user input
+    user_input = input("Please enter your choice: ")
+
+    if user_input == "1":
+        # Ask for dates
+        start_date, end_date = get_dates()
+        # convert to timestamp
+        start_timestamp = convert_to_timestamp(start_date)
+        end_timestamp = convert_to_timestamp(end_date)
+        fetch_data(start_timestamp, end_timestamp)
+        print("Done!")
+
+    elif user_input == "2":
+        # check if data directory has at least one file
+        if not os.listdir(DATA_DIR):
+            print("No data found! Please fetch data first")
+        # Get win percent and loss percent
+        win_percent = input("Please enter the win percent: (5) ")
+        if not win_percent:
+            win_percent = 5
+        loss_percent = input("Please enter the loss percent: (2) ")
+        if not loss_percent:
+            loss_percent = 2
+
+        # Ask for verbosity level
+        verbose = input("Display daily results? (y/n): (n) ")
+        VERBOSE = verbose == "y"
+
+        # Ask for interactive mode
+        interactive = input("Interactive mode? (y/n): (n) ")
+        INTERACTIVE = interactive == "y"
+
+        backtest(
+            win_percent,
+            loss_percent,
+        )
+
+    elif user_input == "3":
+        print("Goodbye!")
+        break
